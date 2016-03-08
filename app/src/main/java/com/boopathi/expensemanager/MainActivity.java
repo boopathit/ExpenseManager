0d466b9894a0620a8b8d7846a9d47a51ec9941f4
Boopathi T <getboopathi.t@gmail.com>
2016-03-08 17:48:32 +0530

    1.Completed Login Activity Layout and Validation process 2.Created Signup Activity with Completed Layout.

--- app/src/main/java/com/boopathi/expensemanager/MainActivity.java
+++ app/src/main/java/com/boopathi/expensemanager/MainActivity.java
@@ -1,14 +1,23 @@
 package com.boopathi.expensemanager;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
+    private static final int REQUEST_SIGNUP = 0;
 
     private EditText _eMailText = (EditText) findViewById(R.id.input_email);
     private EditText _passwordText = (EditText) findViewById(R.id.input_passsword);
@@ -24,9 +33,101 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
+        _loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
 
+        _signUpLink.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent signUpIntent = new Intent(getApplicationContext(), SignUpActivity.class);
+                startActivityForResult(signUpIntent, REQUEST_SIGNUP);
+            }
+        });
 
     }
+
+    private void login() {
+        Log.d(TAG, "Login Method");
+
+        if(!validate()){
+            onLoginFailed();
+            return;
+        }
+
+        _loginButton.setEnabled(false);
+        final ProgressDialog loginProgressDialog = new ProgressDialog(MainActivity.this,R.style.AppTheme_Dark_Dialog);
+        loginProgressDialog.setIndeterminate(true);
+        loginProgressDialog.setMessage("Authenticating Login");
+        loginProgressDialog.show();
+        String email = _eMailText.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+
+                    @Override
+                    public void run() {
+                        onLoginSuccess();
+//                        onLoginFailed()
+                        loginProgressDialog.dismiss();
+                    }
+                }, 3000);
+    }
+
+    private boolean validate() {
+        boolean valid = true;
+
+        String email= _eMailText.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        if(email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            _eMailText.setError("Enter a Valid Email Address");
+            valid= false;
+        } else {
+            _eMailText.setError(null);
+        }
+        if(email.isEmpty() || password.length() < 4 || password.length() > 10 ){
+            _passwordText.setError("Length between 4 and 10 Alphanumeric Characters");
+            valid= false;
+        } else {
+            _passwordText.setError(null);
+        }
+        return valid;
+    }
+
+    private void onLoginFailed() {
+        Toast.makeText(getBaseContext(), "Login Failed", Toast.LENGTH_LONG).show();
+        _loginButton.setEnabled(true);
+    }
+
+    private void onLoginSuccess() {
+        _loginButton.setEnabled(true);
+        finish();
+    }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == REQUEST_SIGNUP){
+            if(resultCode == RESULT_OK) {
+
+//                Implementing Sign up Logic Here
+
+                this.finish();
+            }
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        moveTaskToBack(true);
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
