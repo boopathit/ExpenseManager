2e37455a3315609e3a169a9960b9cc8b595a9e71
Boopathi T <getboopathi.t@gmail.com>
2016-03-10 10:38:24 +0530

    Addding Category

---
+++ app/src/main/java/com/boopathi/expensemanager/db/CategoryNew.java
@@ -0,0 +1,40 @@
+package com.boopathi.expensemanager.db;
+
+/**
+ * Created by Boopathi on 3/9/2016.
+ */
+public class CategoryNew {
+
+    int id;
+    String cat_name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMode() {
+        return mode;
+    }
+
+    public void setMode(String mode) {
+        this.mode = mode;
+    }
+
+    String mode;
+
+    public void category (){
+
+    }
+
+    public String getCat_name() {
+        return cat_name;
+    }
+
+    public void setCat_name(String cat_name) {
+        this.cat_name = cat_name;
+    }
+}
---
+++ app/src/main/java/com/boopathi/expensemanager/db/Transnew.java
@@ -0,0 +1,78 @@
+package com.boopathi.expensemanager.db;
+
+/**
+ * Created by Boopathi on 3/9/2016.
+ */
+public class Transnew {
+
+    int id,type,cat_id,amt;
+    String from,to,date,note;
+
+    public Transnew(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getCat_id() {
+        return cat_id;
+    }
+
+    public void setCat_id(int cat_id) {
+        this.cat_id = cat_id;
+    }
+
+    public int getAmt() {
+        return amt;
+    }
+
+    public void setAmt(int amt) {
+        this.amt = amt;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+}
